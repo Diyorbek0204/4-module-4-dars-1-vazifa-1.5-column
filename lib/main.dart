85d5515993a106e7c2c01c00da826d7bf6fb2241
Diyorbek Primqulov <diyorbekprimqulov334@gmail.com>
2026-02-17 08:48:03 +0500

    my home work

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [MyBox(), MyBox(), MyBox(), MyBox()],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget MyBox() {
+  return Expanded(
+    flex: 1,
+    child: Container(
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(50.0),
+      ),
+    ),
+  );
+}
